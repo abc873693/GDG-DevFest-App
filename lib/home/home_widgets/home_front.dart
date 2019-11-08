3c939f9765d2f90c1762476b1d75e44a4b556634
RainVisitor <abc873693@gmail.com>
2019-11-09 02:10:43 +0800

    Update to flutter web support plugin version

--- lib/home/home_widgets/home_front.dart
+++ lib/home/home_widgets/home_front.dart
@@ -36,11 +36,7 @@ class HomeFront extends StatelessWidget {
       ];
 
   _launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+    await launch(url);
   }
 
   Widget actions(context) => Wrap(
